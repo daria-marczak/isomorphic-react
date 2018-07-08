cabe52929d3367f307e26ac4d2b8beb10bbb3876
daria marczak <marczak.ria@gmail.com>
2018-07-08 15:40:38 +0200

    Implemented server

--- server/index.jsx
+++ server/index.jsx
@@ -1,5 +1,5 @@
 import express from "express";
-import yields from "express-yields";
+import yields from "express-yields"; // Just by importing it it actually works with express 
 import fs from "fs-extra";
 
 const port = process.env.PORT || 3000;
