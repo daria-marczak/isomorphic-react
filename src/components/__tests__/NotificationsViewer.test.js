137d56237d4f78c79952cf561c625945659f63c0
daria marczak <marczak.ria@gmail.com>
2018-07-14 14:23:49 +0200

    Added first tist for stateful component

---
+++ src/components/__tests__/NotificationsViewer.test.js
@@ -0,0 +1,18 @@
+import NotificationsViewer from "../NotificationsViewer";
+import renderer from "react-test-renderer";
+import React from "react";
+import delay from "redux-saga";
+
+describe("the notification viewer", () => {
+  it("should display the correct number of notifications", async() => {
+    const tree = renderer.create(
+      <NotificationsViewer />
+    );
+
+    await delay();
+    const instance = tree.root;
+    const component = instance.findByProps({className: "notifications"});
+    const text = component.children[0];
+    expect(text).toEqual("5 notifications");
+  });
+});
